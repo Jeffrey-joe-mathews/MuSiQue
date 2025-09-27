b5afd8abcd121361fae5caf14f484af11a1210c4
Jeffrey-joe-mathews <jeffreyjoemathews@gmail.com>
2025-09-27 13:24:33 +0530

    added some changes to the drawer menu

--- lib/components.dart/my_drawer.dart
+++ lib/components.dart/my_drawer.dart
@@ -8,10 +8,49 @@ class MyDrawer extends StatelessWidget {
     return Drawer(
       backgroundColor: Theme.of(context).colorScheme.surface,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           // Logo
+          DrawerHeader(child: Icon(
+            Icons.multitrack_audio_sharp,
+            size: 45,
+            color: Theme.of(context).colorScheme.inversePrimary
+            )
+          ),
+
+
+          // homw
+          Padding(
+            padding: const EdgeInsets.only(top: 72, bottom: 0),
+            child: ListTile(
+              title: Center(
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Icon(Icons.home),
+                    const SizedBox(width: 8,),
+                    const Text("H o M e")
+                  ],
+                ),
+              ),
+              onTap:() => Navigator.pop(context),
+            ),
+          ),
 
           // settings
+          ListTile(
+            title: Center(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Icon(Icons.settings),
+                  const SizedBox(width: 8,),
+                  const Text("S e T t I n G s")
+                ],
+              ),
+            ),
+            onTap: (){},
+          )
 
           // profile
         ],
