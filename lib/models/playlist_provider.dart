d2ff0f0919ec147a3ad8719e920ad141e32d558b
Jeffrey-joe-mathews <jeffreyjoemathews@gmail.com>
2025-10-13 19:48:29 +0530

    made the music player finally read a few songs and play them

--- lib/models/playlist_provider.dart
+++ lib/models/playlist_provider.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:musique/models/my_song.dart';
@@ -7,13 +8,121 @@ class PlaylistProvider extends ChangeNotifier {
   List<Song> _playlist = [];
   
   int? _currentSongIndex;
+
+  /* AUDIO PLAYER */
+  // audio player
+  final AudioPlayer _audioPlayer = AudioPlayer();
+
+  // durations
+  Duration _currentDuration = Duration.zero;
+  Duration _totalDuration = Duration.zero;
+
+  // constructor
+  PlaylistProvider() {
+    listenToDuration();
+  }
+
+  // initially not playing or playing
+  bool _isPlaying = false;
+
+  // play the song
+  void play() async {
+    final String path = _playlist[_currentSongIndex!].audioPath;
+    _audioPlayer.stop();
+    _audioPlayer.play(AssetSource(path));
+    _isPlaying = true;
+    notifyListeners();
+  }
+
+  // puase current song
+  void pause() async {
+    _audioPlayer.pause();
+    _isPlaying = false;
+    notifyListeners();
+  }
+
+  // resume playing
+  void resume() async {
+    _audioPlayer.resume();
+    _isPlaying = true;
+    notifyListeners();
+  }
+
+  // pause / play
+  void pauseOrPlay() async {
+    if (_isPlaying) {
+      pause();
+    }
+    else {
+      resume();
+    }
+    notifyListeners();
+  }
+
+
+  // skip previous
+  void skipPrevious() async {
+    if (_playlist.isEmpty || _currentSongIndex == null) return;
+    if (_currentDuration.inSeconds < 5) {
+        currentSongIndex = (_currentSongIndex! - 1)%_playlist.length;
+    }
+    else {
+      _currentDuration = Duration.zero;
+    }
+    notifyListeners();
+  }
+
+  // skip next
+  void skipNext() {
+    if(_currentSongIndex != null && _playlist.isNotEmpty) {
+      currentSongIndex = (_currentSongIndex! + 1) % _playlist.length;
+    }
+  }
+
+  // seek to a specific position within the song
+  void seek (Duration position) async {
+    await _audioPlayer.seek(position);
+  }
+
+  // listen to duration
+  void listenToDuration () {
+    _audioPlayer.onDurationChanged.listen((newDuration) {
+      _totalDuration = newDuration;
+      notifyListeners();
+    });
+
+    _audioPlayer.onPositionChanged.listen((newPosition) {
+      _currentDuration = newPosition;
+      notifyListeners();
+    });
+
+    _audioPlayer.onPlayerComplete.listen((event) {
+      skipNext();
+    });
+  }
+
+  // dispose audio player
+  void disposePlayer () async {
+    _audioPlayer.dispose();
+    notifyListeners();
+    
+  }
+
   // getters
   List<Song> get playlist => _playlist;
   int? get currentSongIndex => _currentSongIndex;
+  bool get isPlaying => _isPlaying;
+  Duration get currentDuration => _currentDuration;
+  Duration get totalDuration => _totalDuration;
 
   //setters
   set currentSongIndex (int? songIndex) {
     _currentSongIndex = songIndex;
+
+    if (songIndex!= null) {
+      play();
+    } 
+
     notifyListeners();
   }
 
--- lib/pages/song_page.dart
+++ lib/pages/song_page.dart
@@ -15,7 +15,13 @@ class _SongPageState extends State<SongPage> {
   Widget build(BuildContext context) {
     return Consumer<PlaylistProvider>(
       builder:
-          (context, value, child) => Scaffold(
+          (context, value, child) {
+
+            final playlist = value.playlist;
+
+            final currentSong = playlist[value.currentSongIndex ?? 0];
+
+            return Scaffold(
             backgroundColor: Theme.of(context).colorScheme.surface,
             body: SafeArea(
               child: Column(
@@ -27,13 +33,13 @@ class _SongPageState extends State<SongPage> {
                       children: [
                         // back button
                         IconButton(
-                          onPressed:() => Navigator.pop(context),
+                          onPressed: () => Navigator.pop(context),
                           icon: const Icon(Icons.arrow_back),
                         ),
 
                         // title of the song
                         Text(
-                          "SoNg NaMe",
+                          "Song Name",
                           style: const TextStyle(
                             fontSize: 16,
                             overflow: TextOverflow.fade,
@@ -49,131 +55,146 @@ class _SongPageState extends State<SongPage> {
                     ),
                   ),
 
-                  Padding(
-                    padding: const EdgeInsets.all(25.0),
-                    child: MusiquePlayer(
-                      child: Column(
-                        children: [
-                          // song image
-                          ClipRRect(
-                            borderRadius: BorderRadiusGeometry.circular(8),
-                            child: Image.asset("assets/images/template.webp"),
-                          ),
+                  Column(
+                    // mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(25.0),
+                        child: MusiquePlayer(
+                          child: Column(
+                            children: [
+                              // song image
+                              ClipRRect(
+                                borderRadius: BorderRadiusGeometry.circular(8),
+                                child: Image.asset(
+                                  currentSong.imagePath,
+                                ),
+                              ),
 
-                          // song and artist name
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                              vertical: 8,
-                              horizontal: 8,
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
+                              // song and artist name
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 8,
+                                  horizontal: 8,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
                                   children: [
-                                    Text(
-                                      "song name",
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 20,
-                                        overflow: TextOverflow.fade,
-                                      ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          currentSong.songName,
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20,
+                                            overflow: TextOverflow.fade,
+                                          ),
+                                        ),
+
+                                        Text(
+                                          currentSong.artistName,
+                                          style: TextStyle(
+                                            overflow: TextOverflow.fade,
+                                          ),
+                                        ),
+                                      ],
                                     ),
 
-                                    Text(
-                                      "artist name",
-                                      style: TextStyle(
-                                        overflow: TextOverflow.fade,
-                                      ),
-                                    ),
+                                    Icon(Icons.favorite, color: Colors.red),
                                   ],
                                 ),
-
-                                Icon(Icons.favorite, color: Colors.red),
-                              ],
-                            ),
+                              ),
+                            ],
                           ),
+                        ),
+                      ),
+
+                      // song duration progress
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          // start time
+                          Text("0:00"),
+                          // shuffle
+                          Icon(Icons.shuffle_rounded),
+                          // repeat
+                          Icon(Icons.repeat, color: Colors.green),
+                          // end time
+                          Text("3:21"),
                         ],
                       ),
-                    ),
-                  ),
 
-                  // song duration progress
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      // start time
-                      Text("0:00"),
-                      // shuffle
-                      Icon(Icons.shuffle_rounded),
-                      // repeat
-                      Icon(Icons.repeat, color: Colors.green,),
-                      // end time
-                      Text("3:21"),
+                      // slider
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 25,
+                          vertical: 8,
+                        ),
+                        child: SliderTheme(
+                          data: SliderThemeData(
+                            thumbShape: SliderComponentShape.noThumb,
+                          ),
+                          child: Slider(
+                            min: 0,
+                            max: 100,
+                            value: 50,
+                            activeColor: Colors.green,
+                            inactiveColor:
+                                Theme.of(context).colorScheme.secondary,
+                            onChanged: (value) {},
+                          ),
+                        ),
+                      ),
+
+                      // controls
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25),
+                        child: Row(
+                          children: [
+                            // prev
+                            Expanded(
+                              child: GestureDetector(
+                                child: MusiquePlayer(
+                                  child: Icon(Icons.skip_previous_outlined),
+                                ),
+                              ),
+                            ),
+
+                            const SizedBox(width: 20),
+                            // play/pause
+                            Expanded(
+                              flex: 2,
+                              child: GestureDetector(
+                                child: MusiquePlayer(
+                                  child: Icon(Icons.play_arrow_outlined),
+                                ),
+                              ),
+                            ),
+
+                            const SizedBox(width: 20),
+                            // next
+                            Expanded(
+                              child: GestureDetector(
+                                child: MusiquePlayer(
+                                  child: Icon(Icons.skip_next_outlined),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
 
-                  // slider
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 8),
-                    child: SliderTheme(
-                      data: SliderThemeData(
-                        thumbShape: SliderComponentShape.noThumb,
-                      ),
-                      child: Slider(
-                        min: 0,
-                        max: 100,
-                        value: 50, 
-                        activeColor: Colors.green,
-                        inactiveColor: Theme.of(context).colorScheme.secondary,
-                        onChanged: (value) {},
-                      ),
-                    ),
-                  ),
-
-                  // controls
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25),
-                    child: Row(
-                      children: [
-                        // prev
-                        Expanded(
-                          child: GestureDetector(
-                            child: MusiquePlayer(
-                              child: Icon(Icons.skip_previous_outlined)
-                            )
-                          ),
-                        ),
-
-                        const SizedBox(width: 20,),
-                        // play/pause
-                        Expanded(
-                          flex: 2,
-                          child: GestureDetector(
-                            child: MusiquePlayer(
-                              child: Icon(Icons.play_arrow_outlined)
-                            )
-                          ),
-                        ),
-
-                        const SizedBox(width: 20,),
-                        // next
-                        Expanded(
-                          child: GestureDetector(
-                            child: MusiquePlayer(
-                              child: Icon(Icons.skip_next_outlined)
-                            )
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-
+                  // till here
                 ],
               ),
             ),
-          ),
+          );
+          }
     );
   }
 }
